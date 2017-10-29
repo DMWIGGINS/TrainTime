40a563ef414d2e76768115fd2e3d2f9087702e94
Doreen Wiggins <Wiggy77@aol.com>
2017-10-29 19:32:17 -0400

    foundation

---
+++ assets/javascript/train.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCuoaga2RX28gNNgUdqELpgCkyHr0qphKU",
+    authDomain: "traintime-19073.firebaseapp.com",
+    databaseURL: "https://traintime-19073.firebaseio.com",
+    projectId: "traintime-19073",
+    storageBucket: "",
+    messagingSenderId: "85912264302"
+};
+firebase.initializeApp(config);
+
